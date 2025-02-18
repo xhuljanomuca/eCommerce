3fdd2fb6f997837768498e8ca6a86d6827ff8891
xhuljanomuca <xhuljano.muca1@gmail.com>
2025-02-18 20:18:06 +0100

    fix

--- myapp/migrations/0002_remove_myuser_bio_remove_myuser_birth_date.py
+++ myapp/migrations/0002_remove_myuser_bio_remove_myuser_birth_date.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ecomerce', '0001_initial'),
+        ('myapp', 'migrations.0001_initial'),
     ]
 
     operations = [
